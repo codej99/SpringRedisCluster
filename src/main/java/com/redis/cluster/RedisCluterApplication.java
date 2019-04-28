63132f092a27c4acbc3eb2c912f8bd685963c2ba
kimyonghwa <codej99@naver.com>
2019-04-29 01:59:24 +0900

    redis initialize

---
+++ src/main/java/com/redis/cluster/RedisCluterApplication.java
@@ -0,0 +1,12 @@
+package com.redis.cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCluterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCluterApplication.class, args);
+    }
+}
