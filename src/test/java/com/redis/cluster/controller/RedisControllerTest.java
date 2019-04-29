8ac867994f616505f57e77f78cdd3cbff6e772aa
kimyonghwa <codej99@naver.com>
2019-04-30 00:49:28 +0900

    cluster test

--- src/test/java/com/redis/cluster/controller/RedisControllerTest.java
+++ src/test/java/com/redis/cluster/controller/RedisControllerTest.java
@@ -1,6 +1,5 @@
 package com.redis.cluster.controller;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
