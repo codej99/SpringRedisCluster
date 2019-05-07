e3e9fcc4c7acbe07ef88b35168e2edab2412a486
kimyonghwa <codej99@nhnent.com>
2019-05-07 19:31:15 +0900

    Spring data redis
    - redis annotation

--- src/main/java/com/redis/cluster/RedisClusterApplication.java
+++ src/main/java/com/redis/cluster/RedisClusterApplication.java
@@ -2,11 +2,13 @@ package com.redis.cluster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisClusterApplication {
 
     public static void main(String[] args) {
