87113e048ba3288afabc2934364b42413a6cf792
kimyonghwa <codej99@nhnent.com>
2019-05-08 18:12:26 +0900

    Spring data redis
    - modify project structure

--- src/main/java/com/redis/cluster/RedisClusterApplication.java
+++ src/main/java/com/redis/cluster/RedisClusterApplication.java
@@ -2,19 +2,11 @@ package com.redis.cluster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RedisClusterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisClusterApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
