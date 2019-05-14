fb4e8bfc419b863340e999fdf70c0c8953ff10e1
kimyonghwa <codej99@nhnent.com>
2019-05-14 17:20:48 +0900

    Delete response body

--- src/main/java/com/redis/cluster/controller/RedisController.java
+++ src/main/java/com/redis/cluster/controller/RedisController.java
@@ -23,21 +23,18 @@ public class RedisController {
     }
 
     @PostMapping("/user")
-    @ResponseBody
     public User postUser(@RequestBody User user) {
         return userJpaRepo.save(user);
     }
 
     @CachePut(value = CacheKey.USER, key = "#user.msrl")
     @PutMapping("/user")
-    @ResponseBody
     public User putUser(@RequestBody User user) {
         return userJpaRepo.save(user);
     }
 
     @CacheEvict(value = CacheKey.USER, key = "#msrl")
     @DeleteMapping("/user/{msrl}")
-    @ResponseBody
     public boolean deleteUser(@PathVariable long msrl) {
         userJpaRepo.deleteById(msrl);
         return true;
